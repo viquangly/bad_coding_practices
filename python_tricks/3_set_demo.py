31b8fd5feec6fcbf7e915d9743be2ec73eb9a6a5
ViLy <viquangly@gmail.com>
2023-05-04 08:59:36 -0400

    Added more set methods.

--- python_tricks/3_set_demo.py
+++ python_tricks/3_set_demo.py
@@ -8,3 +8,17 @@ big_set = set(big_list)
 %%timeit
 500_000 in big_set
 
+bills = {'Josh Allen', 'Jordan Poyer', 'Stefon Diggs', 'Micah Hyde', 'Sean McDermott', 'Damar Hamlin'}
+golfers = {'Josh Allen', 'Jordan Poyer', 'Stefon Diggs', 'Tiger Woods', 'Jack Nicklaus', 'Kevin Chang', 'Tyler Rinker'}
+
+bills - golfers
+golfers - bills
+
+bills.intersection(golfers)
+bills & golfers
+
+bills.union(golfers)
+bills | golfers
+
+bills.symmetric_difference(golfers)
+
