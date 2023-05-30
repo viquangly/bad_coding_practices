88c24c0bbc09e4b547f567e9c8d87ae9cb05a072
ViLy <viquangly@gmail.com>
2023-05-30 08:12:19 -0400

    Added get_email_domain examples for using str arguments for conditionals

--- bad_practices/verbose_conditionals.py
+++ bad_practices/verbose_conditionals.py
@@ -25,3 +25,22 @@ def is_list_empty_better(some_list: List) -> bool:
 
 def is_list_empty_best(some_list: List) -> bool:
     return bool(some_list)
+
+
+def get_email_domain_bad1(email: str, case: str) -> str:
+    domain = email.split('@')[-1]
+    return domain.upper() if case.lower() == 'upper' else domain
+
+
+def get_email_domain_bad2(email: str, upper_case: str) -> str:
+    domain = email.split('@')[-1]
+
+    if upper_case.lower()[0] == 'y':
+        return domain.upper()
+    else:
+        return domain
+
+
+def get_email_domain_better(email: str, upper_case: bool) -> str:
+    domain = email.split('@')[-1]
+    return domain.upper() if upper_case else domain
